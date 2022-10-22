72c4a259bdd00a097cf1805fbf77366c06a6bccd
NawidT <nawidtahmid@gmail.com>
2022-10-22 14:15:56 -0400

    issuefeedcard

--- pages/userFeed.js
+++ pages/userFeed.js
@@ -1,7 +1,33 @@
-import { Heading } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import IssueFeedCard from "../comps/issueFeedCard";
 
 const UserFeed = () => {
-  return <Heading>User Feed</Heading>;
+  const [feed, setFeed] = useState([]);
+  const [userData, setUserData] = useState({});
+
+  return (
+    <Box className="Header" color={"blackAlpha.700"}>
+      <Flex
+        className="HeaderBox"
+        bg="tomato"
+        color={"black"}
+        h="10vh"
+        w="100vw"
+      ></Flex>
+      <Flex
+        className="UserFeed"
+        flexDirection="column"
+        overflowY="scroll"
+        justifyContent="center"
+      >
+        <IssueFeedCard userType={"user"} />
+        <IssueFeedCard userType={"user"} />
+        <IssueFeedCard userType={"user"} />
+        <IssueFeedCard userType={"user"} />
+      </Flex>
+    </Box>
+  );
 };
 
 export default UserFeed;
