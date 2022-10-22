da2015806d1665e99481e4760694c41279d6864f
tawsifkamal <tawsifkamal123@gmail.com>
2022-10-21 22:11:40 -0400

    adding mongo stuff

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import dbConnect from "../../lib/dbConnect";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  try {
+    await dbConnect();
+    res.status(200).send("database works!");
+  } catch (err) {
+    console.log(err);
+  }
 }
