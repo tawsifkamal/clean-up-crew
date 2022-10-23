ffca3c9a22a8956ac07603894f778f9e28cb4408
NawidT <nawidtahmid@gmail.com>
2022-10-22 23:25:12 -0400

    nawid is my name

---
+++ pages/api/post/post-accept.js
@@ -0,0 +1,23 @@
+import dbConnect from "../../../lib/dbConnect";
+const Post = require("../../../lib/models/Post");
+const User = require("../../../lib/models/User");
+
+export default async function handler(req, res) {
+    try {
+        if (req.method !== 'PUT') {
+            res.status(405).json({message: "please use PUT"})
+        } else {
+            await dbConnect();
+            const {userId, postId} = req.body;
+            const user = await User.findById(userId);
+            const post = await Post.findById(postId);
+            if (post.postState == 'open') {
+                post.contractor = userId;
+                post.postState = 'pending'
+            }
+            await post.save();
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
